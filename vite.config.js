3c8add8cc5dc5d8d977ed6b193235d412151d94a
v-i-j-ay <kvijayramareddy@gmail.com>
2026-02-26 16:38:11 +0530

    pwa added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "BuildSetu",
+        short_name: "BuildSetu",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/icon-512.png", sizes: "512x512", type: "image/png" }
+        ]
+      }
+    })
+  ]
+});
